1797ed06e08316d3eea408a70a2684d6b5f59f57
vagrant <vagrant@vagrant.vm>
2018-06-22 22:39:55 +0000

    Funciones definidas

--- init.py
+++ init.py
@@ -3,10 +3,24 @@ from pprint import pprint
 
 class Block():
     """docstring for Block."""
-    def __init__(self, length, height):
+    def __init__(self, length, height, initial):
         self.length = length
         self.height = height
-        self.IsInitial = False
+        self.IsInitial = initial
+
+class Contenedor():
+    """docstring for Contenedor."""
+    def __init__(self, dim):
+        self.dimensiones = dim;
+        self.Space = Matrix = [[0 for x in range(dim)] for y in range(1000)]
+
+class PosicionInidice():
+    """docstring for PosicionInidice."""
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
 
 def ProcessBlocks(rawDataBlocks):
     blocks = []
@@ -15,14 +29,50 @@ def ProcessBlocks(rawDataBlocks):
     return blocks
 
 
+def PosicionValida(bloque, x, y):
+    if contenedor[x][y] == None:
+        return True
+    return False
+
+def AgregarBloque(bloque, Xinicial, Yinicial):
+    for x in range(bloque.length - 1)
+        for y in range(bloque.height - 1):
+            contenedor[Xinicial + x][Yinicial + y] = \
+            Block(bloque.length, bloque.height, False)
+
+    contenedor [Xinicial] [Yinicial] = Block(bloque.length, bloque.height, True);
+
+
+def HayEspacioDisponible():
+    pass
+
+def EncontrarPosicionDisponible(bloque):
+    for linea in contenedor.Space
+        for indice in linea:
+            if HayEspacioDisponible(bloque)
+                return PosicionInidice(linea, indice)
+    return PosicionInidice(-1, -1)
+
+
+def IntentarAgregarBloque(bloque):
+    posicion = EncontrarPosicionDisponible(bloque)
+    if posicion.x == -1 || posicion.y == -1:
+        return false;
+    AgregarBloque(bloque);
+
+
+
 with open('products.json') as f:
     data = json.load(f)
 
+global contenedor;
+contenedor = Contenedor(10);
 blocks = ProcessBlocks(data["Blocks"])
-
 
 for b in blocks:
         print("Length" , b.length, ":" , b.height)
+
+pprint(contenedor);
 
 
 
