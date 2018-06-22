cdbf793795bbc3181c3f9733f5b01488ac413a97
vagrant <vagrant@vagrant.vm>
2018-06-22 21:26:23 +0000

    Parse complete

--- init.py
+++ init.py
@@ -1,14 +1,31 @@
 import json
 from pprint import pprint
 
+class Block():
+    """docstring for Block."""
+    def __init__(self, length, height):
+        self.length = length
+        self.height = height
+        self.IsInitial = False
+
+def ProcessBlocks(rawDataBlocks):
+    blocks = []
+    for b in rawDataBlocks:
+        blocks.append(Block(int(b["Length"]), int(b["Height"])))
+    return blocks
+
+
 with open('products.json') as f:
     data = json.load(f)
 
-blocks = data["Blocks"]
+blocks = ProcessBlocks(data["Blocks"])
+
+
+for b in blocks:
+        print("Length" , b.length, ":" , b.height)
+
+
 
 #print(data["maps"][1]["id"])  # will return 'blabla'
 #print(data["masks"]["id"])    # will return 'valore'
 #print(data["om_points"])      # will return 'value'
-
-for block in blocks:
-        print(int(block["Length"] ) + 5)
